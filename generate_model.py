4138933b473a0b71b95ea7b0a80c760a37fbf8c4
qiaorui <qiaorui.xiang@gmail.com>
2019-04-25 13:56:36 +0200

    create models

--- generate_model.py
+++ generate_model.py
@@ -12,12 +12,85 @@ import seaborn as sns
 import numpy as np
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from sklearn.decomposition import PCA
+import statsmodels.api as sm
+
+
+def convert_to_sequence(df, output_columns, lags=0, aheads=1, dropnan=True):
+    new_df = pd.DataFrame()
+    x_columns = []
+    # Add lags (t-lag, t-lag+1, t-lag+2, ... , t-1)
+    for lag in range(lags, 0, -1):
+        for column in df.columns:
+            new_column_name = column+"_lag_"+str(lag)
+            new_df[new_column_name] = df[column].shift(lag).values
+            x_columns.append(new_column_name)
+    # Add current observation (t)
+    for column in df.columns:
+        new_df[column] = df[column].values
+        x_columns.append(column)
+    # Add ste aheads (t+1, t+2, ... , t+aheads)
+    y_columns = []
+    for ahead in range(1, aheads+1, 1):
+        for output_column in output_columns:
+            new_column_name = output_column+"_ahead_"+str(ahead)
+            new_df[new_column_name] = df[output_column].shift(-ahead).values
+            y_columns.append(new_column_name)
+    if dropnan:
+        new_df.dropna(inplace=True)
+    return new_df
 
 
 def pca(df, tv):
 
     y = df[tv]
     x = df.drop(tv, axis=1)
+
+    N = 300
+    plt.plot(y.tail(N).index, y.tail(N))
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+    decomposition = sm.tsa.seasonal_decompose(y.tail(N), model='additive')
+    fig = decomposition.plot()
+    plt.show()
+
+    plt.figure(figsize=(8, 8))
+    ax1 = plt.subplot(6, 1, 1)
+    plt.plot(x.tail(N).index, x.tail(N)['Temperature'])
+    plt.ylabel("Temp(Cº)")
+    # make these tick labels invisible
+    plt.setp(ax1.get_xticklabels(), visible=False)
+
+    ax2 = plt.subplot(6, 1, 2, sharex=ax1)
+    plt.plot(x.tail(N).index, x.tail(N)['Humidity'])
+    plt.ylabel("Hum(%)")
+    # make these tick labels invisible
+    plt.setp(ax2.get_xticklabels(), visible=False)
+
+    ax3 = plt.subplot(6, 1, 3, sharex=ax1)
+    plt.plot(x.tail(N).index, x.tail(N)['Time_Fragment_Cos'])
+    plt.ylabel("TF(Cos)")
+    # make these tick labels invisible
+    plt.setp(ax3.get_xticklabels(), visible=False)
+
+    ax4 = plt.subplot(6, 1, 4, sharex=ax1)
+    plt.plot(x.tail(N).index, x.tail(N)['Time_Fragment_Sin'])
+    plt.ylabel("TF(Sin)")
+    plt.setp(ax4.get_xticklabels(), visible=False)
+
+    ax5 = plt.subplot(6, 1, 5, sharex=ax1)
+    plt.plot(x.tail(N).index, x.tail(N)['Weekday_Cos'])
+    plt.ylabel("Weekday(Cos)")
+    plt.setp(ax5.get_xticklabels(), visible=False)
+
+    plt.subplot(6, 1, 6, sharex=ax1)
+    plt.plot(x.tail(N).index, x.tail(N)['Condition_Good'])
+    plt.ylabel("Condition")
+    plt.xticks(x.tail(N).index.normalize().unique(), x.tail(N).index.normalize().unique().day)
+
+    plt.subplots_adjust(hspace = .001)
+    plt.tight_layout()
+    plt.show()
 
     # Using Pearson Correlation
     plt.figure(figsize=(12, 10))
@@ -33,18 +106,6 @@ def pca(df, tv):
     plt.semilogy(fit.explained_variance_ratio_.cumsum(), '--o', label="Cumulative Explained Variance Ratio")
     plt.legend()
     plt.show()
-
-    cols = list(x.columns)
-    model = LinearRegression()
-    # Initializing RFE model
-    rfe = RFE(model, 7)
-    # Transforming data using RFE
-    X_rfe = rfe.fit_transform(x, y)
-    # Fitting the data to model
-    model.fit(X_rfe, y)
-    temp = pd.Series(rfe.support_, index=cols)
-    selected_features_rfe = temp[temp == True].index
-    print(selected_features_rfe)
 
 
 def prepare_data(df, weather_data, time_slot):
@@ -84,7 +145,9 @@ def prepare_data(df, weather_data, time_slot):
     data.loc[data['Weekday'] == 6, 'Weekday'] += 2
 
     data['Weekday_Cos'] = np.cos(2*np.pi/10 * (data['Weekday']-2))
+    data['Weekday_Sin'] = np.sin(2*np.pi/10 * (data['Weekday']-2))
     data['Month_Cos'] = np.cos(2*np.pi/12 * (data['Month']))
+    data['Month_Sin'] = np.sin(2 * np.pi / 12 * (data['Month']))
     data['Time_Fragment_Cos'] = np.cos(2*np.pi/(data['Time_Fragment'].max()+1) * (data['Time_Fragment']))
     data['Time_Fragment_Sin'] = np.sin(2 * np.pi / (data['Time_Fragment'].max() + 1) * (data['Time_Fragment']))
 
@@ -98,6 +161,8 @@ def prepare_data(df, weather_data, time_slot):
     data['Month_Cos'] = data['Month_Cos'].astype(np.float16)
     data['Time_Fragment_Cos'] = data['Time_Fragment_Cos'].astype(np.float16)
     data['Time_Fragment_Sin'] = data['Time_Fragment_Sin'].astype(np.float16)
+    data['Weekday_Sin'] = data['Weekday_Sin'].astype(np.float16)
+    data['Month_Sin'] = data['Month_Sin'].astype(np.float16)
 
     max_count = data['Count'].max()
     if max_count < (1 << 7):
@@ -117,6 +182,7 @@ def main():
     parser.add_argument("-ct", default="cleaned_data/JC_trip_data.csv", help="input cleaned trip data path")
     parser.add_argument("-ot", type=int, help="Outlier threshold")
     parser.add_argument("-ts", type=int, default=30, help="Time slot for the aggregation, units in minute")
+    parser.add_argument("-tp", type=float, default=0.2, help="Test size percentage for split the data")
     parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
 
     args = parser.parse_args()
@@ -124,6 +190,7 @@ def main():
     weather_data_path = args.cw
     trip_data_path = args.ct
     time_slot = args.ts
+    test_pct = args.tp
 
     pd.set_option('display.precision', 3)
     pd.set_option('display.max_columns', 500)
@@ -133,6 +200,8 @@ def main():
 
     trip_data = utils.read_cleaned_trip_data(trip_data_path)
     assert trip_data is not None
+
+    assert 24*60 % time_slot == 0
 
     if args.ot is not None:
         print("Removing outlier with threshold", args.ot)
@@ -144,26 +213,30 @@ def main():
     trip_data = trip_data[trip_data.End_Station_ID.isin(start_stations_ids)]
 
     print("Breaking trip data to pick-up data and drop-off data")
-    # pick_ups, drop_offs = utils.break_up(trip_data)
-
     pick_ups = trip_data[['Start_Station_ID', 'Start_Time']].copy()
-    drop_offs = trip_data[['End_Station_ID', 'Stop_Time']].copy()
+    #drop_offs = trip_data[['End_Station_ID', 'Stop_Time']].copy()
 
     del trip_data
     gc.collect()
 
     pick_ups.rename(columns={"Start_Station_ID": "Station_ID", "Start_Time": "Timestamp"}, inplace=True)
-    drop_offs.rename(columns={"Stop_Station_ID": "Station_ID", "Stop_Time": "Timestamp"}, inplace=True)
+    #drop_offs.rename(columns={"Stop_Station_ID": "Station_ID", "Stop_Time": "Timestamp"}, inplace=True)
 
-    busiest_station = pick_ups["Station_ID"].value_counts().idxmax()
+    th_day = pick_ups['Timestamp'].max().value - (pick_ups['Timestamp'].max().value - pick_ups['Timestamp'].min().value) * test_pct
+    th_day = pd.to_datetime(th_day).normalize()
 
     data = prepare_data(pick_ups, weather_data, time_slot)
 
-    pca_data = data.loc[data["Station_ID"]==busiest_station].drop("Station_ID", axis=1)
-
-    pca(pca_data, 'Count')
+    busiest_station = pick_ups["Station_ID"].value_counts().idxmax()
     # PCA
+    pca_data = data.loc[data["Station_ID"]==busiest_station].drop("Station_ID", axis=1)
+    pca(pca_data, 'Count')
 
+    dg = data.groupby("Station_ID")
+    for id, df in dg:
+        sequence = convert_to_sequence(df.drop(columns=['Holiday', 'Station_ID']), ['Count'], 3, 4)
+        print(sequence)
+        exit(1)
     # Training modules, train data by different techniques
     print("{0:*^80}".format(" Training "))
 
---
+++ scripts/models.py
@@ -0,0 +1,57 @@
+from . import utils
+
+
+class Model:
+    def __init__(self):
+        print("Creating LTSM model")
+        self.data = None
+
+
+class HA(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating HA model")
+        self.data = None
+
+
+class LTSM(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+        self.data = None
+
+
+class MLP(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+        self.data = None
+
+
+class LR(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+        self.data = None
+
+
+class ARIMA(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+        self.data = None
+
+
+class SSA(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+        self.data = None
+
+
+class VAR(Model):
+    def __init__(self):
+        super().__init__()
+        print("Creating LTSM model")
+
+

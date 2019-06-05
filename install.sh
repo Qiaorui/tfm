76570a8eb1ab1d38ddbc1f38eb0677f90df0f09a
qiaorui <qiaorui.xiang@gmail.com>
2019-06-05 13:47:53 +0200

    prepare install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if command -v python3 &>/dev/null; then
+    echo Python 3 is installed
+else
+    echo Python 3 is not installed
+    exit
+fi
+
+pip install -r requirements.txt
+
+echo Instalation completed
